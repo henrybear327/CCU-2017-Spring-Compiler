22049682747ae441707cbf18c761ba14e16bd68e
Henry Tseng <henrybear327@gmail.com>
2017-05-31 08:48:35 +0800

    Finish lexer

---
+++ proj3/CactusParser.java
@@ -0,0 +1,214 @@
+// Generated from Cactus.g4 by ANTLR 4.7
+import org.antlr.v4.runtime.atn.*;
+import org.antlr.v4.runtime.dfa.DFA;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.misc.*;
+import org.antlr.v4.runtime.tree.*;
+import java.util.List;
+import java.util.Iterator;
+import java.util.ArrayList;
+
+@SuppressWarnings({"all", "warnings", "unchecked", "unused", "cast"})
+public class CactusParser extends Parser {
+	static { RuntimeMetaData.checkVersion("4.7", RuntimeMetaData.VERSION); }
+
+	protected static final DFA[] _decisionToDFA;
+	protected static final PredictionContextCache _sharedContextCache =
+		new PredictionContextCache();
+	public static final int
+		T__0=1, DIGIT=2, WHITESPACE=3;
+	public static final int
+		RULE_token = 0, RULE_dot = 1;
+	public static final String[] ruleNames = {
+		"token", "dot"
+	};
+
+	private static final String[] _LITERAL_NAMES = {
+		null, "'.'"
+	};
+	private static final String[] _SYMBOLIC_NAMES = {
+		null, null, "DIGIT", "WHITESPACE"
+	};
+	public static final Vocabulary VOCABULARY = new VocabularyImpl(_LITERAL_NAMES, _SYMBOLIC_NAMES);
+
+	/**
+	 * @deprecated Use {@link #VOCABULARY} instead.
+	 */
+	@Deprecated
+	public static final String[] tokenNames;
+	static {
+		tokenNames = new String[_SYMBOLIC_NAMES.length];
+		for (int i = 0; i < tokenNames.length; i++) {
+			tokenNames[i] = VOCABULARY.getLiteralName(i);
+			if (tokenNames[i] == null) {
+				tokenNames[i] = VOCABULARY.getSymbolicName(i);
+			}
+
+			if (tokenNames[i] == null) {
+				tokenNames[i] = "<INVALID>";
+			}
+		}
+	}
+
+	@Override
+	@Deprecated
+	public String[] getTokenNames() {
+		return tokenNames;
+	}
+
+	@Override
+
+	public Vocabulary getVocabulary() {
+		return VOCABULARY;
+	}
+
+	@Override
+	public String getGrammarFileName() { return "Cactus.g4"; }
+
+	@Override
+	public String[] getRuleNames() { return ruleNames; }
+
+	@Override
+	public String getSerializedATN() { return _serializedATN; }
+
+	@Override
+	public ATN getATN() { return _ATN; }
+
+	public CactusParser(TokenStream input) {
+		super(input);
+		_interp = new ParserATNSimulator(this,_ATN,_decisionToDFA,_sharedContextCache);
+	}
+	public static class TokenContext extends ParserRuleContext {
+		public List<TerminalNode> DIGIT() { return getTokens(CactusParser.DIGIT); }
+		public TerminalNode DIGIT(int i) {
+			return getToken(CactusParser.DIGIT, i);
+		}
+		public List<DotContext> dot() {
+			return getRuleContexts(DotContext.class);
+		}
+		public DotContext dot(int i) {
+			return getRuleContext(DotContext.class,i);
+		}
+		public List<TerminalNode> WHITESPACE() { return getTokens(CactusParser.WHITESPACE); }
+		public TerminalNode WHITESPACE(int i) {
+			return getToken(CactusParser.WHITESPACE, i);
+		}
+		public TokenContext(ParserRuleContext parent, int invokingState) {
+			super(parent, invokingState);
+		}
+		@Override public int getRuleIndex() { return RULE_token; }
+		@Override
+		public void enterRule(ParseTreeListener listener) {
+			if ( listener instanceof CactusListener ) ((CactusListener)listener).enterToken(this);
+		}
+		@Override
+		public void exitRule(ParseTreeListener listener) {
+			if ( listener instanceof CactusListener ) ((CactusListener)listener).exitToken(this);
+		}
+	}
+
+	public final TokenContext token() throws RecognitionException {
+		TokenContext _localctx = new TokenContext(_ctx, getState());
+		enterRule(_localctx, 0, RULE_token);
+		int _la;
+		try {
+			enterOuterAlt(_localctx, 1);
+			{
+			setState(9);
+			_errHandler.sync(this);
+			_la = _input.LA(1);
+			while ((((_la) & ~0x3f) == 0 && ((1L << _la) & ((1L << T__0) | (1L << DIGIT) | (1L << WHITESPACE))) != 0)) {
+				{
+				setState(7);
+				_errHandler.sync(this);
+				switch (_input.LA(1)) {
+				case DIGIT:
+					{
+					setState(4);
+					match(DIGIT);
+					}
+					break;
+				case T__0:
+					{
+					setState(5);
+					dot();
+					}
+					break;
+				case WHITESPACE:
+					{
+					setState(6);
+					match(WHITESPACE);
+					}
+					break;
+				default:
+					throw new NoViableAltException(this);
+				}
+				}
+				setState(11);
+				_errHandler.sync(this);
+				_la = _input.LA(1);
+			}
+			}
+		}
+		catch (RecognitionException re) {
+			_localctx.exception = re;
+			_errHandler.reportError(this, re);
+			_errHandler.recover(this, re);
+		}
+		finally {
+			exitRule();
+		}
+		return _localctx;
+	}
+
+	public static class DotContext extends ParserRuleContext {
+		public DotContext(ParserRuleContext parent, int invokingState) {
+			super(parent, invokingState);
+		}
+		@Override public int getRuleIndex() { return RULE_dot; }
+		@Override
+		public void enterRule(ParseTreeListener listener) {
+			if ( listener instanceof CactusListener ) ((CactusListener)listener).enterDot(this);
+		}
+		@Override
+		public void exitRule(ParseTreeListener listener) {
+			if ( listener instanceof CactusListener ) ((CactusListener)listener).exitDot(this);
+		}
+	}
+
+	public final DotContext dot() throws RecognitionException {
+		DotContext _localctx = new DotContext(_ctx, getState());
+		enterRule(_localctx, 2, RULE_dot);
+		try {
+			enterOuterAlt(_localctx, 1);
+			{
+			setState(12);
+			match(T__0);
+			}
+		}
+		catch (RecognitionException re) {
+			_localctx.exception = re;
+			_errHandler.reportError(this, re);
+			_errHandler.recover(this, re);
+		}
+		finally {
+			exitRule();
+		}
+		return _localctx;
+	}
+
+	public static final String _serializedATN =
+		"\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\3\5\21\4\2\t\2\4\3"+
+		"\t\3\3\2\3\2\3\2\7\2\n\n\2\f\2\16\2\r\13\2\3\3\3\3\3\3\2\2\4\2\4\2\2\2"+
+		"\21\2\13\3\2\2\2\4\16\3\2\2\2\6\n\7\4\2\2\7\n\5\4\3\2\b\n\7\5\2\2\t\6"+
+		"\3\2\2\2\t\7\3\2\2\2\t\b\3\2\2\2\n\r\3\2\2\2\13\t\3\2\2\2\13\f\3\2\2\2"+
+		"\f\3\3\2\2\2\r\13\3\2\2\2\16\17\7\3\2\2\17\5\3\2\2\2\4\t\13";
+	public static final ATN _ATN =
+		new ATNDeserializer().deserialize(_serializedATN.toCharArray());
+	static {
+		_decisionToDFA = new DFA[_ATN.getNumberOfDecisions()];
+		for (int i = 0; i < _ATN.getNumberOfDecisions(); i++) {
+			_decisionToDFA[i] = new DFA(_ATN.getDecisionState(i), i);
+		}
+	}
+}
